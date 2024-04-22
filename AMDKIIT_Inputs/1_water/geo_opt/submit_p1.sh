5834d0701cc00cb6bd93f3657f8e9bd309f973d7
mscc07 <153798678+mscc07@users.noreply.github.com>
2024-04-22 10:59:26 +0530

    Add files via upload

---
+++ AMDKIIT_Inputs/1_water/geo_opt/submit_p1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --job-name=amdkiit_h2o
+#SBATCH -o ./%x.%j.out
+#SBATCH -e ./%x.%j.err
+#SBATCH --output=log.o%j
+#SBATCH --partition=standard
+#SBATCH --nodes=2
+#SBATCH --tasks-per-node=48
+#SBATCH --time=00:15:00
+#SBATCH --exclusive=user
+
+export OMPI_MCA_mtl=psm2
+module load cdac/MSCC/amdkiit
+
+INPUT=input.yaml
+OUTPUT=amdkiit13.out
+MYNP=48
+
+echo " 2 nodes"
+
+time mpirun -n 96 --npernode 48 amdkiit.x $INPUT > $OUTPUT
