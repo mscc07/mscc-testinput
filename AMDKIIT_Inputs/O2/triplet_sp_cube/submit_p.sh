4851b699b8400fad9e2c827d551f4e8c740fe6a6
mscc07 <153798678+mscc07@users.noreply.github.com>
2024-04-24 14:24:10 +0530

    Update submit_p.sh

--- AMDKIIT_Inputs/O2/triplet_sp_cube/submit_p.sh
+++ AMDKIIT_Inputs/O2/triplet_sp_cube/submit_p.sh
@@ -7,16 +7,11 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=48
 #SBATCH --time=00:15:00
-#SBATCH --export=NONE
-#SBATCH --mail-type=NONE
-#SBATCH --no-requeue
+#SBATCH --reservation=mscc_workshop
 
-module load openmpi/openmpi_4.1.2
-module load compiler/gcc/8.3.0 
-
-export AMD=/home/msccp23/amdkiit-main/source/build/amdkiit.x
+module load cdac/MSCC/amdkiit
 
 INPUT=input.yaml
 OUTPUT=amdkiit.out
 MYNP=1
-mpirun -n ${MYNP} $AMD $INPUT > $OUTPUT
+mpirun -n ${MYNP} amdkiit.x $INPUT > $OUTPUT
